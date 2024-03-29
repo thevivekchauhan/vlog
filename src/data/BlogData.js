2e5d4ee18110ad934f736e7942b36a2d19fe8681
thevivekchauhan <165350621+thevivekchauhan@users.noreply.github.com>
2024-03-29 11:12:53 +0530

    initial commit

---
+++ src/data/BlogData.js
@@ -0,0 +1,67 @@
+export const Blogs = [
+    {
+        id:1,
+        name:"Vintage Car Museum, At Ahmedabad.",
+        tags:["oldcars","museum","Ahmedabad","TheVivekChauhan"],
+        date:"31 December, 2023",
+        imgSrc:"https://ahmedabadtourism.in/images//tourist-places/auto-world-vintage-car-museum-ahmedabad/auto-world-vintage-car-museum-ahmedabad-tourism-entry-ticket-price.jpg",
+        link:"https://www.youtube.com/watch?v=PlIsDROPTJA&ab_channel=TheVivekChauhan"
+    },
+    {
+        id:2,
+        name:"Sidi Saiyyed Mosque, Ahmedabad.",
+        tags:["Ahmedabad","SidiSaiyyedMosque","TheVivekChauhan"],
+        date:"11 December, 2023",
+        imgSrc:"https://www.gujarattourism.com/content/dam/gujrattourism/images/heritage-sites/siddi-sayed-mosque/gallery/Siddi%20Sayed%20Mosque4.jpg",
+        link:"https://www.youtube.com/watch?v=N9kTLPbCVrQ&ab_channel=TheVivekChauhan"
+    },
+    {
+        id:3,
+        name:"Sabarmati Ashram Part - 1",
+        tags:["Ahmedabad","SabarmatiAshram", "TheVivekChauhan"],
+        date:"3 November, 2023",
+        imgSrc:"https://resize.indiatv.in/resize/newbucket/1200_675/2023/08/collage-maker-13-aug-2023-01-15-pm-1813-1691912842.webp",
+        link:"https://www.youtube.com/watch?v=Z6uNV_Snc74&ab_channel=TheVivekChauhan"
+    },
+    {
+        id:4,
+        name:"Sabarmati Ashram Part - 2",
+        tags:["Ahmedabad","SabarmatiAshram","TheVivekChauhan"],
+        date:"4 November, 2023",
+        imgSrc:"https://cdn.thewire.in/wp-content/uploads/2021/08/05110859/49937193313_3fd84b2668_c.jpg",
+        link:"https://www.youtube.com/watch?v=mYEI38wfb90&ab_channel=TheVivekChauhan"
+    },
+    {
+        id:5,
+        name:"England vs Australia Cricket world cup",
+        tags:["Cricket","EnglandvsAustralia", "World-cup", "TheVivekChauhan"],
+        date:"4 November, 2023",
+        imgSrc:"https://d16f573ilcot6q.cloudfront.net/wp-content/uploads/2023/06/ENG-vs-AUS-416x234.jpeg",
+        link:"https://www.youtube.com/watch?v=Odw3JlFwUiM&t=25s&ab_channel=TheVivekChauhan"
+    },
+    {
+        id:6,
+        name:"Trimandir temple, Ahmedabad.",
+        tags:["TheVivekChauhan","Trimandirtemple", "Ahmedabad"],
+        date:"9 December, 2023",
+        imgSrc:"https://lh3.googleusercontent.com/p/AF1QipOXFj-QxBQ7RFeEc5wC1Gs0mgEAeWAidD-K8xcF=s1360-w1360-h1020",
+        link:"https://www.youtube.com/watch?v=QYliKpFNU3o&ab_channel=TheVivekChauhan"
+    },
+    {
+        id:7,
+        name:"Datar hill vlog",
+        tags:["Hills","TheVivekChauhan","DatarVlog"],
+        date:" 5 December 2023",
+        imgSrc:"https://www.trodly.com/pictures/attraction/5859.jpg",
+        link:"https://www.youtube.com/watch?v=ZsloAsZG5b8&ab_channel=TheVivekChauhan"
+    },
+    {
+        id:8,
+        name:"Junagadh Gujarat Lili parikrama [ Big adventure ]",
+        tags:["parikrama","TheVivekChauhan","Junagadh","Gujarat"],
+        date:"3 December 2023",
+        imgSrc:"https://blog.yatradham.org/wp-content/uploads/2023/10/Girnar-Parikrama-1.jpg",
+        link:"https://www.youtube.com/watch?v=NUyUbVr_AQw&t=40s&ab_channel=TheVivekChauhan"
+    },
+  
+] 
---
+++ src/data/WorkData.js
@@ -0,0 +1,62 @@
+export const Work = [
+    {
+        id:1,
+        name:"Medical Store",
+        description:"When I finished BCA then I want to go abroad for my higher education but it was unsuccessful, then I was just thinking I should doing Master in India but it will start june 2024 so, I have free time then I was join medical store.",
+        tags:["Firstjob","Medical"],
+        
+        // demo:"https://agency-website-eta.vercel.app/",
+        // github:"https://github.com/codebucks27/Agency-website"
+    },
+    {
+        id:2,
+        name:"Vijay Sells",
+        description:"I was doing job in Apple Department in Vijay sells because they gave me more salary as compared to medical",
+        tags:["Secondjob","Apple","Vijaysells"],
+        
+        // demo:"http://react-job-listing-website.codebucks27.vercel.app/",
+        // github:"https://github.com/codebucks27/react-job-listing-website"
+        
+    },
+    {
+        id:3,
+        name:"BitBeast Company ",
+        description:"One day I was thinking Medical and Vijay sells is not my field so, I should join IT field. That's why I join this company.",
+        tags:["Thirdjob","Developer","IT"],
+        
+        // demo:"http://devto-clone-codebucks27.vercel.app/",
+        // github:"https://github.com/codebucks27/devto-clone"
+    },
+    {
+        id:4,
+        name:"In Future",
+        description:"Right now, I am working in BitBeast company. In future I will doing new then I will tell you...",
+        tags:["BrightFuture","Billionaire","VC"],
+        
+        // demo:"https://react-sidebar.vercel.app/",
+        // github:"https://github.com/codebucks27/react-sidebar"
+       
+    },
+    // {
+    //     id:5,
+    //     name:"Todo App(React + Redux)",
+    //     description:"A todo app build using Redux-toolkit and framer-motion for smooth animations.",
+    //     tags:["react","redux","framer-motion"],
+        
+    //   demo:"http://react-redux-todo-app-git-main-codebucks27.vercel.app/",
+    //      github:"https://github.com/codebucks27/React-Redux-Todo-App"
+        
+    // },
+    // {
+    //     id:6,
+    //     name:"Responsive Navigation Menu",
+    //     description:"A reponsive navbar menu component in ReactJS with react-router functionalities. Built from scratch.",
+    //     tags:["react","react-router","css"],
+        
+    //     demo:"http://react-responsive-navbar-codebucks27.vercel.app/",
+    //     github:"https://github.com/codebucks27/React-responsive-navbar"
+        
+    // },
+    
+  
+] 
